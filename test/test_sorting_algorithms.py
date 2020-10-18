e1e5ffcd9a3c1c485e5dd8ca3a7dba72ce6b5d07
Celal ORMANLI <ormanlicelal@gmail.com>
2020-10-19 00:36:50 +0300

    add 2 sorting algoritihm with their unit tests

---
+++ test/test_sorting_algorithms.py
@@ -0,0 +1,12 @@
+from src.sorting_algorithms import SortingAlgorithms
+
+def test_sorting_algorithims_selection_sort():
+    sorting_algorithims=SortingAlgorithms()
+    assert sorting_algorithims.selection_short([10,8,48])==[8,10,48]
+    assert sorting_algorithims.selection_short([10,8,48,1])==[1,8,10,48]
+
+def test_sorting_algorithims_bubble_sort():
+    sorting_algorithims=SortingAlgorithms()
+    assert sorting_algorithims.bubble_sort([10,8,48])==[8,10,48]
+    assert sorting_algorithims.bubble_sort([10,8,48,1])==[1,8,10,48]
+    assert sorting_algorithims.bubble_sort([-1,2,3,4])==[-1,2,3,4]
