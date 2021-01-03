ef32b0092a0b35713cf7af806c3f795c21ae19f2
Celal ORMANLI <ormanlicelal@gmail.com>
2021-01-04 00:58:57 +0300

    pep8

--- src/sorting_algorithms.py
+++ src/sorting_algorithms.py
@@ -7,14 +7,14 @@ class SortingAlgorithms:
                     min_num_ind = y
             temp = input_array[x]
             input_array[x] = input_array[min_num_ind]
-            input_array[min_num_ind] = temp 
+            input_array[min_num_ind] = temp
         return input_array
- 
+
     def bubble_sort(self, input_array):
-        for x in range(len(input_array)-1):
-            for y in range(len(input_array)-x-1):
-                if input_array[y]>input_array[y+1]:
-                    temp=input_array[y]
-                    input_array[y] = input_array[y+1]
-                    input_array[y+1] = temp
+        for x in range(len(input_array) - 1):
+            for y in range(len(input_array) - x - 1):
+                if input_array[y] > input_array[y + 1]:
+                    temp = input_array[y]
+                    input_array[y] = input_array[y + 1]
+                    input_array[y + 1] = temp
         return input_array
