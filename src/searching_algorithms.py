effe1b12dcb988153f1e3fa8dde6e99359ffd5d8
Celal ORMANLI <ormanlicelal@gmail.com>
2021-01-07 23:26:01 +0300

    binary search

--- src/searching_algorithms.py
+++ src/searching_algorithms.py
@@ -4,3 +4,16 @@ class SearchingAlgorithms:
             if searched_element == x:
                 return True
         return False
+
+    def binary_search(self, input_array, le, ri, x):
+        while le <= ri:
+            mid = le + (ri - 1) // 2
+
+            if input_array[mid] == x:
+                return True
+            elif input_array[mid] < x:
+                le = mid + 1
+            else:
+                ri = mid - 1
+
+        return False
--- test/test_searching_algorithms.py
+++ test/test_searching_algorithms.py
@@ -5,3 +5,11 @@ def test_linear_search():
     searching_algorithms = SearchingAlgorithms()
     assert searching_algorithms.linear_search([10, 8, 48], 5) is False
     assert searching_algorithms.linear_search([10, 8, 48], 48) is True
+
+
+def test_binary_search():
+    searching_algorithms = SearchingAlgorithms()
+    assert searching_algorithms.binary_search(
+        [8, 13, 58, 96, 101], 0, 4, 5) is False
+    assert searching_algorithms.binary_search(
+        [8, 13, 58, 96, 101], 0, 4, 96) is True
