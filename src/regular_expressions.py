3cb445890ae0e427f1337cd759771f63c9c73368
Celal ORMANLI <ormanlicelal@gmail.com>
2021-06-13 22:52:17 +0300

    regex for numbers, amounts, plate numbers

--- src/regular_expressions.py
+++ src/regular_expressions.py
@@ -11,3 +11,23 @@ class RegularExpressions:
         if re.search(regex, input_text):
             return True
         return False
+
+    def find_numbers(self, input_text):
+        regex = r'\d+'
+        return re.findall(regex, input_text)
+
+    def find_senders(self, input_text):
+        regex = r'Sender (.*) Recievers'
+        return re.findall(regex, input_text)
+
+    def find_amounts(self, input_text):
+        regex = r'[0-9]+\$'
+        return re.findall(regex, input_text)
+
+    def find_amounts(self, input_text):
+        regex = r'[0-9]+\$'
+        return re.findall(regex, input_text)
+
+    def find_plate_numbers(self, input_text):
+        regex = r'\d{2}\s?\S{1,3}[A-Z]\s?\d{2,4}'
+        return re.findall(regex, input_text)
--- test/test_regular_expressions.py
+++ test/test_regular_expressions.py
@@ -11,3 +11,27 @@ def test_validete_email():
     regular_expressions = RegularExpressions()
     assert regular_expressions.validete_email('test@test.net')
     assert regular_expressions.validete_email('test@emailcom') == False
+
+
+def test_find_numbers():
+    regular_expressions = RegularExpressions()
+    assert regular_expressions.find_numbers(
+        'dskhjvadsl kjsşakjfşaksljdflşksajşfk jadsşlkfj  asd 76868 fksdljfkl  4654665') == ['76868', '4654665']
+
+
+def test_find_senders():
+    regular_expressions = RegularExpressions()
+    assert regular_expressions.find_senders(
+        'gsdklgdsj lk jdsşlk Sender test sender Recievers dsfjdslkfjşdslk') == ['test sender']
+
+
+def test_find_amounts():
+    regular_expressions = RegularExpressions()
+    assert regular_expressions.find_amounts(
+        'dskhjvadsl kjsşak125$jfşaksljdflşksajşfk jadsşlkfj  asd 76868 fksdljfkl  50$ 4654665') == ['125$', '50$']
+
+
+def test_find_plate_numbers():
+    regular_expressions = RegularExpressions()
+    assert regular_expressions.find_plate_numbers(
+        'dskhjvadsl kjsşakjfşaks73AS585l73jdflşksajşfk jadsşlkfj  asd 53225358 dasdsdas 04 SL 945 jdslkjflsd') == ['73AS585', '04 SL 945']
